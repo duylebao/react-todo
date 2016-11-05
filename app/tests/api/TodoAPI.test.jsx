b15d6167b1c296facc94b56ef2dbc5beb445bb8f
Duy Le <dle@walmartlabs.com>
2016-11-04 22:36:45 -0700

    add filter

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -52,4 +52,48 @@ describe('TodoAPI', () => {
     });
   });
 
+  describe('filterTodos', () => {
+    var todos = [
+      {
+        id: 1,
+        text: 'some text here',
+        completed: true
+      },
+      {
+        id: 2,
+        text: 'other text here',
+        completed: false
+      },
+      {
+        id: 3,
+        text: 'some text here',
+        completed: true
+      }
+    ];
+
+    it('should return all items if show completed is true', () =>{
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return only non-completed items when showCompleted is false', () =>{
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () =>{
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should return all items searchText is empty', () =>{
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return all items searchText is match', () =>{
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(2);
+    });
+  });
 });
