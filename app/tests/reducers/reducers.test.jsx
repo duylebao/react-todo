044b61eca06a7b7062271bfd6250a6d7a9b8c592
Duy Le <dle@walmartlabs.com>
2016-11-06 22:35:19 -0800

    update todos on firebase

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -59,7 +59,7 @@ describe('Reducers', () =>{
       expect(res[0]).toEqual(todos[0]);
     });
 
-    it('should toggle todo', () => {
+    it('should update todo', () => {
       var todos = [{
         id: '123',
         text: 'something',
@@ -68,13 +68,20 @@ describe('Reducers', () =>{
         completedAt: 125
       }];
 
+      var updates = {
+        completed: false,
+        completedAt: null
+      };
+
       var action = {
-        type: 'TOGGLE_TODO',
-        id: '123'
+        type: 'UPDATE_TODO',
+        id: todos[0].id,
+        updates
       }
       var res = reducers.todosReducer(df(todos), df(action));
-      expect(res[0].completed).toEqual(false);
-      expect(res[0].completedAt).toEqual(undefined);
+      expect(res[0].completed).toEqual(updates.completed);
+      expect(res[0].completedAt).toEqual(updates.completedAt);
+      expect(res[0].text).toEqual(todos[0].text);
     });
   });
 
